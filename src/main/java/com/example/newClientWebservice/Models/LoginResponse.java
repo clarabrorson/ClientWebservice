92dcb13eda1b7ee60e62a26f7f816c148d89f962
clarabrorson <88clabro@gafe.molndal.se>
2023-12-14 09:38:43 +0100

    Update LoginResponse.java

--- src/main/java/com/example/newClientWebservice/Models/LoginResponse.java
+++ src/main/java/com/example/newClientWebservice/Models/LoginResponse.java
@@ -1,4 +1,14 @@
 package com.example.newClientWebservice.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
+
+        private User user;
+        private String jwt;
 }
