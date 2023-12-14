8c420b658146c684b0991bcd18df503db0ffaa72
Jafar Hussein <Jafarhussein6@gmail.com>
2023-12-14 09:18:30 +0100

    Fixade repo

---
+++ src/main/java/com/example/newClientWebservice/NewClientWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.newClientWebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewClientWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewClientWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newClientWebservice/NewClientWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newClientWebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewClientWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
