cd90b5e87c4a01579ee1dbd4ed40bcf91d14cbfe
Fredrik <91frerin@gafe.molndal.se>
2023-12-15 11:47:08 +0100

    Skapat Article-model-klassen

---
+++ src/main/java/com/example/newClientWebservice/Models/Article.java
@@ -0,0 +1,19 @@
+package com.example.newClientWebservice.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+
+
+    private Long id;
+    private String name;
+    private Double cost;
+    private String description;
+    private Integer quantity;
+
+}
