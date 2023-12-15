4076a63d4844e1324ab1d73adc58a7ab5218d0de
Fredrik <91frerin@gafe.molndal.se>
2023-12-15 12:45:35 +0100

    Börjat på ArticelService-klassen

--- src/main/java/com/example/newClientWebservice/Service/ArticleService.java
+++ src/main/java/com/example/newClientWebservice/Service/ArticleService.java
@@ -1,4 +1,21 @@
 package com.example.newClientWebservice.Service;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
 public class ArticleService {
+
+    private static final CloseableHttpClient httpClient = HttpClients.createDefault();
+
+    public static void getAllArticles() {
+
+        HttpGet request = new HttpGet("http://localhost:8081/article");
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        
+    }
+
 }
