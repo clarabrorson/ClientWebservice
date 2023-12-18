9b39721ced71bb97105837017faf455569cbdd83
Fredrik <91frerin@gafe.molndal.se>
2023-12-18 14:32:49 +0100

    getOneBook-metoden

--- src/main/java/com/example/newClientWebservice/NewClientWebserviceApplication.java
+++ src/main/java/com/example/newClientWebservice/NewClientWebserviceApplication.java
@@ -1,13 +1,22 @@
 package com.example.newClientWebservice;
 
+import com.example.newClientWebservice.Service.ArticleService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NewClientWebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewClientWebserviceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ArticleService articleService) {
+		return args -> {
+		};
 	}
 
 }
--- src/main/java/com/example/newClientWebservice/Service/ArticleService.java
+++ src/main/java/com/example/newClientWebservice/Service/ArticleService.java
@@ -11,19 +11,19 @@ import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.HttpHeaders;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Service
 public class ArticleService {
 
     private static final CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    public static ArrayList<Article> getAllArticles(String jwt) throws IOException, ParseException {
+    public static ArrayList<Article> getAllArticles() throws IOException, ParseException {
 
-        HttpGet request = new HttpGet("http://localhost:8081/article");
-
-        request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
+        HttpGet request = new HttpGet("http://localhost:8081/articles");
 
         CloseableHttpResponse response = httpClient.execute(request);
 
@@ -38,8 +38,26 @@ public class ArticleService {
         ArrayList<Article> articles = mapper.readValue(EntityUtils.toString(entity), new TypeReference<ArrayList<Article>>() {});
 
         for (Article article : articles) {
-            System.out.println(String.format("Artikeln %s kostar %s", article.getName(), article.getCost()));
+            System.out.println(String.format("Artikel: %s \n Pris: %.2f \n ", article.getName(), article.getCost()));
         }
         return articles;
+    }
+
+    public static void getOneArticle(int id) throws IOException, ParseException {
+
+        HttpGet request = new HttpGet(String.format("http://localhost:8081/articles/%d", id));
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        if (response.getCode() != 200) {
+            System.out.println("Error uppstod");
+            return;
+        }
+        HttpEntity entity = response.getEntity();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Article article = mapper.readValue(EntityUtils.toString(entity), new TypeReference<Article>() {});
+
+        System.out.println(String.format("Artikeln %s kostar %.2f", article.getName(), article.getCost()));
     }
 }
