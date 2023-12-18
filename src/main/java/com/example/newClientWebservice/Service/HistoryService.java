4dbf7a852c6d40c86fb6f58ebc077044c6c83b12
Jafar Hussein <Jafarhussein6@gmail.com>
2023-12-18 14:21:29 +0100

    Fixade historyService

--- src/main/java/com/example/newClientWebservice/Service/HistoryService.java
+++ src/main/java/com/example/newClientWebservice/Service/HistoryService.java
@@ -26,27 +26,33 @@ public class HistoryService {
         //skapa ett objekt av http get klassen
         HttpGet request = new HttpGet("http://localhost:8081/webshop/history");
 
-        //Inludera en Authorization metod till request
+        // Include an Authorization method to the request
         request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
 
-        // Exekvera Request
+        // Execute the Request
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             if (response.getCode() != 200) {
                 System.out.println("Error");
                 return null;
             }
 
-            //Visa upp response payload i console
+            // Display the response payload in the console
             HttpEntity entity = response.getEntity();
 
-            //Konvertera ResponsePayload till användbart objekt.
+            // Convert ResponsePayload to a useful object.
             ObjectMapper mapper = new ObjectMapper();
             ArrayList<History> histories = mapper.readValue(EntityUtils.toString(entity), new TypeReference<ArrayList<History>>() {
             });
 
+            System.out.println("Histories:");
+            // Print out the histories in the list
             for (History history : histories) {
-                System.out.println(String.format("Article: %s", history.getPurchasedArticles()));
+                System.out.println(String.format(
+                        "  History id: %d, totalCost: %d, user: %s",
+                        history.getId(), history.getTotalCost(), history.getUser()
+                ));
             }
+
             return histories;
         } catch (IOException | ParseException e) {
             e.printStackTrace();
@@ -54,33 +60,44 @@ public class HistoryService {
         }
     }
 
-    public static ArrayList<History> getCurrentUserHistory(String jwt){
+    public static ArrayList<History> getCurrentUserHistory(String jwt) {
         HttpGet request = new HttpGet("http://localhost:8081/webshop/history/currentUserHistory");
 
-        //Inludera en Authorization metod till request
+        // Include an Authorization method to the request
         request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
-        // Exekvera Request
+
+        // Execute the Request
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             if (response.getCode() != 200) {
                 System.out.println("Error");
                 return null;
             }
 
-            //Visa upp response payload i console
+            // Display the response payload in the console
             HttpEntity entity = response.getEntity();
 
-            //Konvertera ResponsePayload till användbart objekt.
+            // Convert ResponsePayload to a useful object.
             ObjectMapper mapper = new ObjectMapper();
-            ArrayList<History> histories = mapper.readValue(EntityUtils.toString(entity), new TypeReference<ArrayList<History>>() {
+            ArrayList<Article> articles = mapper.readValue(EntityUtils.toString(entity), new TypeReference<ArrayList<Article>>() {
             });
 
-            for (History history : histories) {
-                System.out.println(String.format("Article: %s", history.getPurchasedArticles()));
+            System.out.println("Purchased Articles:");
+            // Print out the articles in the list
+            for (Article article : articles) {
+                System.out.println(String.format(
+                        "  Article id: %d, name: %s, cost: %f, description: %s, quantity: %d",
+                        article.getId(), article.getName(), article.getCost(), article.getDescription(), article.getQuantity()
+                ));
             }
-            return histories;
+
+            return null;  // Update this to return the actual list of histories
         } catch (IOException | ParseException e) {
             e.printStackTrace();
             return null;
         }
+    }
+    public static void main(String[] args) {
+        String jwt = "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJzZWxmIiwic3ViIjoiYWRtaW4iLCJpYXQiOjE3MDI5MDUzMTksInJvbGVzIjoiQURNSU4ifQ.bgeiYV_AxaJggsxUT9S7_0wYKv2dvX59CsX0zf_WmLYLoXe-Kn_LnYHSHRy9NL_aw5jxykNwQUiDLhkoeVL9jdjwNEvPJO1laOmqxEP-JGQhmveBa80UxRbOknpswC77sCZAp54PrqB8YPx4R-F1ZLWvEi3dCW2vXN9b522Y8crHIwxYp13CyPWVtvnXBDZzyT7YP2vozssAFOHyH95dOy6pC_m312pOvYXxDhIyipED0pPKmeRWNWSy5LNg1vYVilHE_OyNv282FzcjMmLaneWWCT510nmtQ4-s7O_bRSNYpfEVnWTamCN0EZVSRfNgIuv0Ul3_d9vtwgu5SJ_vUw";
+        getAllHistory(jwt);
     }
 }
