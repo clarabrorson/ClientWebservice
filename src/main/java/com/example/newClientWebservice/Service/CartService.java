7d7a3834bed0c715c66dd8093437611aa1b59929
Jafar Hussein <Jafarhussein6@gmail.com>
2023-12-22 16:07:27 +0100

    Fixade purchase

--- src/main/java/com/example/newClientWebservice/Service/CartService.java
+++ src/main/java/com/example/newClientWebservice/Service/CartService.java
@@ -1,6 +1,7 @@
 package com.example.newClientWebservice.Service;
 
 import com.example.newClientWebservice.Models.Cart;
+import com.example.newClientWebservice.Models.History;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpDelete;
@@ -88,7 +89,7 @@ public class CartService {
         ObjectMapper mapper = new ObjectMapper();
         Cart cart = mapper.readValue(EntityUtils.toString(entity), new TypeReference<Cart>() {});
 
-        System.out.println(String.format("Cart %s belongs to %s and contains %s", cart.getId(), cart.getUser(), cart.getArticles()));
+        System.out.println(String.format("Cart %s belongs to %s and contains %s", cart.getId(), cart.getUsername(), cart.getArticles()));
     }
 
     public static void updateArticleCount(int cartId, int articleId, int quantity, String jwt) throws IOException, ParseException {
@@ -135,17 +136,38 @@ public class CartService {
         //Skriv ut cart och artikel som har tagits bort
         System.out.println(String.format("Article %s has been deleted from cart %s", articleId, cartId));
     }
+    public static void purchaseArticles(String jwt) {
+        try {
+            HttpPost request = new HttpPost("http://localhost:8081/webshop/history/purchase");
+
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                if (response.getCode() != 200) {
+                    System.out.println(response.getCode());
+                    System.out.println("Error");
+                    return;
+                }
+
+                System.out.println("Purchase completed successfully.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public static void main(String[] args) throws IOException, ParseException {
 
    String jwt = String.valueOf(login().getJwt());
 
-            getAllCarts(jwt);
-            getOneCartById(1, jwt);
-            addArticleToCart(1, 1, jwt);
-            updateArticleCount(1, 1, 5, jwt);
-            deleteArticleFromCart(1, 1, jwt);
+
+//            getOneCartById(1, jwt);
+         addArticleToCart(4, 1, jwt);
+        getAllCarts(jwt);
+//            purchaseArticles(jwt);
+//            updateArticleCount(1, 1, 5, jwt);
+//            deleteArticleFromCart(1, 1, jwt);
 
     }
 
