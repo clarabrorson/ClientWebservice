605c9e518cc26d7d1e70dc1cc249b381639cb711
Jafar Hussein <Jafarhussein6@gmail.com>
2023-12-28 21:29:24 +0100

    Fixade kommentar

--- src/main/java/com/example/newClientWebservice/Menu/MainMenu.java
+++ src/main/java/com/example/newClientWebservice/Menu/MainMenu.java
@@ -4,24 +4,54 @@ import com.example.newClientWebservice.Service.UtilService;
 import org.apache.hc.core5.http.ParseException;
 
 import java.io.IOException;
+/**
+ * @Author Jafar Hussein
+ * den här klassen är en hub som leder till andra klasser
+ * @Method runMeny är en metod som kör alla andra metoder i en metod som beter sig som en hub för att leda till andra metoder
+ * @Method printMainMenu skriver ut menyn
+ * @Method userChoice är en metod för en switch case som leder till andra metoder
+ **/
 
 public class MainMenu {
-   private static boolean isRunning = true;
-    public static void run() throws IOException, ParseException {
+   private static boolean isRunning = true; // global variabel så att vi kan använda den för att stänga av programmet
+
+    /**
+     * @Method runMeny är en metod som kör alla andra metoder i en metod som beter sig som en hub för att leda till andra metoder
+     * @return void
+     * @Error Exception e är för att fånga alla fel som kan uppstå
+     */
+
+    public static void runMeny(){
         while (isRunning) {
-            printMainMenu();
-            int choice = UtilService.getIntInput("Enter your choice: ");
+            try {
+                printMainMenu();
+            int choice = UtilService.getIntInput("Enter your choice: \n");
             userChoice(choice);
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a number.\n");
+            }
 
         }
     }
-
+    /**
+     * @Method printMainMenu skriver ut menyn
+     * @return void
+     * @Array menuItems är en array av string som innehåller alla val i menyn
+     * @for loop för att skriva ut alla val i menyn
+     */
     private static void printMainMenu() {
         String [] menuItems = {"1. Login", "2. Register","3. View articles", "4. Exit"};
         for (String menuItem : menuItems) {
             System.out.println(menuItem);
         }
     }
+
+    /**
+     * @Method userChoice är en metod för en switch case som leder till andra metoder
+     * @return void
+     * @param choice är en int variabel som tar emot användarens val
+     * @switch case för att leda till andra metoder
+     */
 
     public static void userChoice(int choice) throws IOException, ParseException {
         switch (choice) {
@@ -48,6 +78,6 @@ public class MainMenu {
     }
 
     public static void main(String[] args) throws IOException, ParseException {
-        run();
+        runMeny();
     }
 }
