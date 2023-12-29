f115c7c7a5e4a4d878550230f81f9ca2cf627654
Jafar Hussein <Jafarhussein6@gmail.com>
2023-12-29 13:47:42 +0100

    Fixade history

--- src/main/java/com/example/newClientWebservice/Menu/Login.java
+++ src/main/java/com/example/newClientWebservice/Menu/Login.java
@@ -37,7 +37,7 @@ public class Login {
                     }
                     break;
                 case 2:
-                    MainMenu.run();
+                    MainMenu.runMeny();
                     return;
                 default:
                     System.out.println("Invalid input. Please enter a number between 1 and 3.");
@@ -48,7 +48,9 @@ public class Login {
 
 
     }
-
+//public static void main(String[] args) throws IOException, ParseException {
+//        loginMenu();
+//    }
 
 
 }
