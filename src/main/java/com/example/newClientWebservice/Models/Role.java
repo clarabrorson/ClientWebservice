ec0ba6006bb46daae0caf820bed8883808d061db
clarabrorson <88clabro@gafe.molndal.se>
2023-12-31 14:35:23 +0100

    Update Role.java
    javadocs

--- src/main/java/com/example/newClientWebservice/Models/Role.java
+++ src/main/java/com/example/newClientWebservice/Models/Role.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Denna klass används för att representera en roll
+ * som används för att ge användare olika behörigheter i applikationen.
+ * @author Clara Brorson
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
